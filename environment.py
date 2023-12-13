ac83e7ab34e966a5ce0a66ff398ff6af7bcb7600
marisaazevedo <marisapsazevedo@gmail.com>
2023-12-13 18:53:39 +0000

    2 million steps

--- environment.py
+++ environment.py
@@ -3,14 +3,16 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 import os
 
-
 env = gym.make('CarRacing-v2', render_mode = 'rgb_array')
 env = DummyVecEnv([lambda: env])
 
 log_path = os.path.join('Train', 'Logs')
 model = PPO('CnnPolicy', env, verbose = 1, tensorboard_log = log_path)
 
-model.learn(total_timesteps = 2000000)
+TIME_STEPS = 1500000
+iters = 0
 
-ppo_path = os.path.join('Train', 'Models', 'PPO_2M')
-model.save(ppo_path)
+while True:
+    iters += 1
+    model.learn(total_timesteps = TIME_STEPS, reset_num_timesteps = False, tb_log_name = 'PPO')
+    model.save('Train/Models/CarRacing-v2_' + str(iters))
